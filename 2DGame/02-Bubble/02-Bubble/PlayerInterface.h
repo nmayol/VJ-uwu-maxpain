3668a87aa66a0936dc7df35d6c62be2bbf42369d
Jorge_Borras <jorge.borras@estudiantat.upc.edu>
2023-11-04 19:22:24 +0100

    Interfaz Finished

--- 2DGame/02-Bubble/02-Bubble/PlayerInterface.h
+++ 2DGame/02-Bubble/02-Bubble/PlayerInterface.h
@@ -12,10 +12,34 @@ public:
 	void render();
 	void setScreenX(const float& new_x);
 
+	void changeActualLevel(const int& new_level);
+
+	void startTime();
+	void stopTime();
+	void changeTickRate(const int& new_tick_rate);
+	bool endedTime();
+
 
 private:
-	glm::vec2 posEntity;
-	glm::ivec2 tileMapDispl;
 	Sprite* backgroundSprite;
 	Texture background;
+
+	vector<Sprite*> score;
+	vector<Sprite*> coins;
+	Sprite* coinSymbol;
+	Sprite* levelSymbol;
+	vector<Sprite*> time;
+	Texture lettersSpritesheet;
+
+	int total_score;
+	int total_coins;
+	int actual_level;
+	int time_left;
+
+	bool time_counting_down;
+	int countdown_frames;
+	int tick_rate;
+
+	void initNumberInScreen(vector<Sprite*>& sprite_vector, const int& n_digits, int number, ShaderProgram& shaderProgram);
+	void updateNumber(vector<Sprite*>& number_in_game, int old_number, int new_number);
 };
